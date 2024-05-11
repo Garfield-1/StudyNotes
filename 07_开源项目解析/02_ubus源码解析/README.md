04b3a093e8d50041cd39720b20e6eae54be288fe
Garfield-1 <44843466+Garfield-1@users.noreply.github.com>
2024-05-11 13:15:10 +0800

    upload

--- 07_开源项目解析/02_ubus源码解析/README.md
+++ 07_开源项目解析/02_ubus源码解析/README.md
@@ -1,1 +1,6 @@
-待完善
+ubus编译依赖json-c和libubox
+
+相关源码下载路径
+git clone https://github.com/json-c/json-c.git
+git clone http://git.openwrt.org/project/libubox.git
+git clone https://git.openwrt.org/project/ubus.git
---
+++ 07_开源项目解析/02_ubus源码解析/jsonC_build/apps/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/qiujian/only_test/ubus/json-c/apps
+# Build directory: /home/qiujian/only_test/ubus/jsonC_build/apps
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
