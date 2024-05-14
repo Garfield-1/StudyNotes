12f884ee738c19333d5cfc34406990058266670a
Garfield-1 <44843466+Garfield-1@users.noreply.github.com>
2024-05-15 00:57:24 +0800

    Update build.sh

--- 07_开源项目解析/02_ubus源码解析/build.sh
+++ 07_开源项目解析/02_ubus源码解析/build.sh
@@ -2,41 +2,95 @@
 
 base_path=$(pwd)
 
-jsonC_path=$(pwd)/json-c
-jsonC_build_path=$(pwd)/jsonC_build
+jsonC_path=$base_path/json-c
+jsonC_build_path=$base_path/jsonC_build
 
-libubox_path=$(pwd)/libubox
-libubox_build_path=$(pwd)/libubox_build
+libubox_path=$base_path/libubox
+libubox_build_path=$base_path/libubox_build
 
-ubus_path=$(pwd)/ubus
-ubus_build_path=$(pwd)/ubus_build
+ubus_path=$base_path/ubus
+ubus_build_path=$base_path/ubus_build
+
+libs_path=$base_path/libs
+depend_libs_path=$base_path/libs/depend_libs
 
 # json-c库编译
-function jsonC_build {
+jsonC_build() {
     echo "Start compiling jsonc"
     
     mkdir $jsonC_build_path
     cd $jsonC_build_path
 
-    $jsonC_path/cmake-configure --prefix=$jsonC_build_path --enable-shared
+    $jsonC_path/cmake-configure --prefix=./ --enable-shared
 
-    make
+    make &
 }
 
 # libubox库编译
-function libubox {
+libubox_build() {
     echo "Start compiling libubox"
+
+    mkdir $libubox_build_path
+    cd $libubox_build_path
+
+    cmake -D BUILD_LUA:BOOL=OFF -D BUILD_EXAMPLES:BOLL=OFF -D BUILD_STATIC=ON $libubox_path
+
+    make &
 }
 
 # ubus库编译
-function ubus {
+ubus_build() {
     echo "Start compiling ubus"
+
+    mkdir $ubus_build_path
+    cd $ubus_build_path
+
+    cmake -D BUILD_STATIC=ON -D ubox_library=$depend_libs_path  $ubus_path
+#  -D UBOX_LIBRARY_PATH=$libubox_build_path -D BLOB_LIBRARY_PATH=$libubox_build_path -D 
+    make &
 }
 
-function clean {
+clean() {
     echo "Clean up old files"
     rm -rf $jsonC_build_path
+    rm -rf $libubox_build_path
 }
 
-clean
-jsonC_build
+copy_libs() {
+    echo "copy_libs"
+    rm -rf $libs_path
+    mkdir -p $depend_libs_path
+    cp $jsonC_build_path/libjson-c* $depend_libs_path
+    cp $libubox_build_path/libubox* $depend_libs_path
+}
+
+build() {
+    jsonC_build
+    libubox_build
+}
+
+# 检查传递的参数并调用相应的函数
+case "$1" in
+    jsonC)
+        jsonC_build
+        ;;
+    libubox)
+        libubox_build
+        ;;
+    ubus)
+        ubus_build
+        ;;
+    all)
+        build
+        ;;
+    copy)
+        copy_libs
+        ;;
+    clean)
+        clean
+        ;;
+    *)
+        echo "Usage: $0 [jsonC|libubox|ubus|all|clean]"
+        exit 1
+        ;;
+esac
