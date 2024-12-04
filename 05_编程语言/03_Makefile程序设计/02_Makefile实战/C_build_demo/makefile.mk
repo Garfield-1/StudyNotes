61dc405c10d505221b536049a5ff6f2d61969650
Garfield-1 <44843466+Garfield-1@users.noreply.github.com>
2024-12-04 11:09:48 +0800

    Update makefile.mk

--- 05_编程语言/03_Makefile程序设计/02_Makefile实战/C_build_demo/makefile.mk
+++ 05_编程语言/03_Makefile程序设计/02_Makefile实战/C_build_demo/makefile.mk
@@ -2,22 +2,24 @@ CC := gcc
 SRCS := $(wildcard ./src/*.c)
 TARGETS := $(patsubst %.c, %.o, $(SRCS))
 CFLAGS := -g -Wall 
-INC_DIR = -I ./inc/						#链接自定义头文件路径
-LIB := -lm								#链接库 
-#LDFLAGS = -L./lib -Wl,-rpath=./lib		#自定义动态库路径，并设置程序允许时从这个路径读取动态库
-PRODUCT := a.out						#编译产物
+INC_DIR = -I ./inc/                     #链接自定义头文件路径
+LIB := -lm                              #链接库 
+LDFLAGS = -L./lib -Wl,-rpath=./lib      #自定义动态库路径，并设置程序允许时从这个路径读取动态库
+EXEC_BIN := a.out                       #编译产物,可执行文件
+EXEC_LIB := liba.so                     #编译产物,动态库
 
-all: $(PRODUCT)
-
-$(PRODUCT): $(TARGETS)
-	$(CC) $^ -o $@ $(LIB)
-#	如果需要使用自定义的动态库，请使用下面的代码
-#	$(CC) $^ -o $@ $(LDFLAGS)
+all: $(EXEC_BIN) $(EXEC_LIB)
 	rm $(TARGETS)
+
+$(EXEC_BIN): $(TARGETS)
+	$(CC) $^ -o $@ $(LDFLAGS) $(LIB)
+	
+$(EXEC_LIB): $(TARGETS)
+	$(CC) -shared -fPIC -o $@ $(TARGETS)
 
 %.o: %.c
 	$(CC) -c $(CFLAGS) $(INC_DIR) $< -o $@
 
 .PHONY: clean
 clean:
-	rm $(TARGETS) $(PRODUCT)
+	rm $(EXEC_BIN) $(EXEC_LIB)
